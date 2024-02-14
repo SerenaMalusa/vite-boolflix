3e957288021aec4a537cea21b64cc765643ff5c8
SerenaMalusa <sere.malu@gmail.com>
2024-02-14 16:41:11 +0100

    feat: create function to generate axios request

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,38 @@
 <script>
 
+  // import axios
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        MovieList: [],
+        title: 'titolo',
+      }
+    },
+    methods: {
+      fetchMovieList() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=875b204ed440a701c5a21db41f3ee0a2&language=it-IT&query=porco rosso')
+          .then((res) => {
+            this.MovieList = res.data.results;
+          });
+      },
+    },
+    created() {
+      console.log(this.title);
+      this.fetchMovieList();
+    },
+  }
+
 </script>
 
 <template>
+
+  <h1>{{ title }}</h1>
+
+  <ul>
+    <li v-for="movie in MovieList"> {{ movie.title }}</li>
+  </ul>
  
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 
 import App from './App.vue';
 
-const app = createApp(app);
+const app = createApp(App);
 app.mount('#app');
