8f2feb15db60090bc8bef24c9193155ea69993fb
SerenaMalusa <sere.malu@gmail.com>
2024-02-14 17:20:07 +0100

    feat: feacth tv series

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
   export default {
     data() {
       return {
-        MovieList: [],
+        MovieList: [], 
+        tvList: [],       
         searchValue: '',
       }
     },
@@ -14,9 +15,16 @@
       fetchMovieList(param) {
         axios.get('https://api.themoviedb.org/3/search/movie?api_key=875b204ed440a701c5a21db41f3ee0a2&language=it-IT&query='+param)
           .then((res) => {
-            this.MovieList = res.data.results;
+              this.MovieList = res.data.results;
           });
       },
+      
+      fetcTvList(param) {
+        axios.get('https://api.themoviedb.org/3/search/tv?api_key=875b204ed440a701c5a21db41f3ee0a2&language=it-IT&query='+param)
+          .then((res) => {
+              this.tvList = res.data.results;
+          });
+      }
     },
     created() {
       // console.log(this.title);
@@ -29,18 +37,34 @@
 <template>
 
   <input type="text" v-model="searchValue"/>
-  <button @click="fetchMovieList(searchValue)">Cerca</button>
+  <button @click="fetchMovieList(searchValue); fetcTvList(searchValue);">Cerca</button>
 
+  <div class="container">
 
-  <div v-for="movie in MovieList"> 
-    <ul>
-      <li>Titolo: {{ movie.title }} </li>
-      <li>Titolo Originale: {{ movie.original_title }} </li>
-      <li>Lingua Originale: {{ movie.original_language }} </li>
-      <li>Voto: {{ movie.vote_average }}/10</li>
-      <li>Anno di Uscita: {{ movie.release_date.substr(0,4) }}</li>
-    </ul>
+    <div> 
+      <h2>Film</h2>
+      <ul v-for="movie in MovieList">
+        <li>Titolo: {{ movie.title }} </li>
+        <li>Titolo Originale: {{ movie.original_title }} </li>
+        <li>Lingua Originale: {{ movie.original_language }} </li>
+        <li>Voto: {{ movie.vote_average }}/10</li>
+        <li>Anno di Uscita: {{ movie.release_date.substr(0,4) }}</li>
+      </ul>
+    </div>
+  
+    <div> 
+      <h2>Serie</h2>
+      <ul v-for="show in tvList">
+        <li>Titolo: {{ show.name }} </li>
+        <li>Titolo Originale: {{ show.original_name }} </li>
+        <li>Lingua Originale: {{ show.original_language }} </li>
+        <li>Voto: {{ show.vote_average }}/10</li>
+        <li>Anno di Uscita: {{ show.first_air_date.substr(0,4) }}</li>
+      </ul>
+    </div>
+
   </div>
+
   
  
 </template>
@@ -49,5 +73,15 @@
 
   // import file general.scss
   @use './styles/general.scss';
+
+  .container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+
+    div {
+      width: 50%;
+    }
+  }
 
 </style>
